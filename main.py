3b187e96f8fc0951f5a0d6c2989c9925c1eb5c9a
Admin <nicholasfran45@gmail.com>
2022-10-29 10:56:08 -0400

    This is the master file

---
+++ main.py
@@ -0,0 +1,77 @@
+# Define the game function
+
+def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 7, 8, 9]):
+    # Print the game board
+    print('# # # # # # #\n'
+          '#', game_array[0], '|', game_array[1], '|', game_array[2], '#\n'
+          '# - - - - - #\n'
+          '#', game_array[3], '|', game_array[4], '|', game_array[5], '#\n'
+          '# - - - - - #\n'
+          '#', game_array[6], '|', game_array[7], '|', game_array[8], '#\n'
+          '# # # # # # #\n')
+
+    # The while statement will keep playing the game until the turns are up
+    while turns < 9:
+        # The for loop alternates between both players
+        for i in range(0, 2):
+            print('Start turn', turns + 1, ': Player', i + 1, '!')
+
+            # The current player will pick their next move on the board by choosing a number on the board
+            selection = int(input('Select Location : '))  # TODO add input validation
+            # This is a git test
+            game_array[selection - 1] = game_pieces[i]  # The board space is replaced with the players game piece
+            print('# # # # # # #\n'  # Print an updated board
+                  '#', game_array[0], '|', game_array[1], '|', game_array[2], '#\n'
+                  '# - - - - - #\n'
+                  '#', game_array[3], '|', game_array[4], '|', game_array[5], '#\n'
+                  '# - - - - - #\n'
+                  '#', game_array[6], '|', game_array[7], '|', game_array[8], '#\n'
+                  '# # # # # # #\n')
+
+            # This next section will check to see if a player won
+
+            if game_array[0] == game_array[1] == game_array[2]:  # Check row 1
+                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+                return
+            elif game_array[3] == game_array[4] == game_array[5]:  # Check row 2
+                print('Player', game_pieces.index(game_array[3]) + 1, 'wins!!!')
+                return
+            elif game_array[6] == game_array[7] == game_array[8]:  # Check row 3
+                print('Player', game_pieces.index(game_array[6]) + 1, 'wins!!!')
+                return
+            elif game_array[0] == game_array[3] == game_array[6]: # Check column 1
+                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+                return
+            elif game_array[1] == game_array[4] == game_array[7]:  # Check column 2
+                print('Player', game_pieces.index(game_array[1]) + 1, 'wins!!!')
+                return
+            elif game_array[2] == game_array[5] == game_array[8]:  # Check column 3
+                print('Player', game_pieces.index(game_array[2]) + 1, 'wins!!!')
+                return
+            elif game_array[0] == game_array[4] == game_array[8]:  # Check diagonal top left to bottom right
+                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+                return
+            elif game_array[2] == game_array[4] == game_array[6]:  # Check diagonal top right to bottom left
+                print('Player', game_pieces.index(game_array[2]) + 1, 'wins!!!')
+                return
+            else:  # If no player one, increment the turn counter and continue the game
+                turns += 1
+
+
+# Begin program
+
+# Print welcome statement
+print('''
+Welcome to Tik Tac Toe!
+Player 1 will be "X"
+Player 2 will be "O"
+Pick a location on the board to place your pieces!
+''')
+
+# Start the game by calling the function
+game_machine()
+
+# Print goodbye statement
+print('Thank you for playing!')
+
+# End program
