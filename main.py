50af3d8db698ae946618511fa8ec31ef94b20698
Admin <nicholasfran45@gmail.com>
2022-11-02 11:59:25 -0400

    Wrapped the game program in a while loop so the players can decide if they want to play again. Changed the game array so it copies the original anytime the game is replayed.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Define the game function
+# Define the game functions
 
 # This function accepts the current game array and prints the game board
 def game_board(board_array=[]):
@@ -14,8 +14,9 @@ def game_board(board_array=[]):
 # This is the function that runs the processes of the game including
 # player input and calling the game board.
 def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 7, 8, 9]):
-    # Print the game board
-    game_board(game_array)
+    # Make a copy of the game array and print the board
+    game_array_copy = game_array[:]
+    game_board(game_array_copy)
 
     # The while statement will keep playing the game until the turns are up
     while turns < 9:
@@ -33,8 +34,8 @@ def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 
 
                         # The board space is replaced with the players game piece and
                         # the updated board is printed for the player.
-                        game_array[int(selection) - 1] = game_pieces[i]
-                        game_board(game_array)
+                        game_array_copy[int(selection) - 1] = game_pieces[i]
+                        game_board(game_array_copy)
                         break
                     else:
                         print('Please select a number on the game board')
@@ -43,46 +44,63 @@ def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 
 
             # This next section will check to see if a player won
             # rolling through all the different winning scenarios.
-            if game_array[0] == game_array[1] == game_array[2]:  # Check row 1
-                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+            if game_array_copy[0] == game_array_copy[1] == game_array_copy[2]:  # Check row 1
+                print('Player', game_pieces.index(game_array_copy[0]) + 1, 'wins!!!')
                 return
-            elif game_array[3] == game_array[4] == game_array[5]:  # Check row 2
-                print('Player', game_pieces.index(game_array[3]) + 1, 'wins!!!')
+            elif game_array_copy[3] == game_array_copy[4] == game_array_copy[5]:  # Check row 2
+                print('Player', game_pieces.index(game_array_copy[3]) + 1, 'wins!!!')
                 return
-            elif game_array[6] == game_array[7] == game_array[8]:  # Check row 3
-                print('Player', game_pieces.index(game_array[6]) + 1, 'wins!!!')
+            elif game_array_copy[6] == game_array_copy[7] == game_array_copy[8]:  # Check row 3
+                print('Player', game_pieces.index(game_array_copy[6]) + 1, 'wins!!!')
                 return
-            elif game_array[0] == game_array[3] == game_array[6]: # Check column 1
-                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+            elif game_array_copy[0] == game_array_copy[3] == game_array_copy[6]: # Check column 1
+                print('Player', game_pieces.index(game_array_copy[0]) + 1, 'wins!!!')
                 return
-            elif game_array[1] == game_array[4] == game_array[7]:  # Check column 2
-                print('Player', game_pieces.index(game_array[1]) + 1, 'wins!!!')
+            elif game_array_copy[1] == game_array_copy[4] == game_array_copy[7]:  # Check column 2
+                print('Player', game_pieces.index(game_array_copy[1]) + 1, 'wins!!!')
                 return
-            elif game_array[2] == game_array[5] == game_array[8]:  # Check column 3
-                print('Player', game_pieces.index(game_array[2]) + 1, 'wins!!!')
+            elif game_array_copy[2] == game_array_copy[5] == game_array_copy[8]:  # Check column 3
+                print('Player', game_pieces.index(game_array_copy[2]) + 1, 'wins!!!')
                 return
-            elif game_array[0] == game_array[4] == game_array[8]:  # Check diagonal top left to bottom right
-                print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
+            elif game_array_copy[0] == game_array_copy[4] == game_array_copy[8]:  # Check diagonal top left to bottom right
+                print('Player', game_pieces.index(game_array_copy[0]) + 1, 'wins!!!')
                 return
-            elif game_array[2] == game_array[4] == game_array[6]:  # Check diagonal top right to bottom left
-                print('Player', game_pieces.index(game_array[2]) + 1, 'wins!!!')
+            elif game_array_copy[2] == game_array_copy[4] == game_array_copy[6]:  # Check diagonal top right to bottom left
+                print('Player', game_pieces.index(game_array_copy[2]) + 1, 'wins!!!')
                 return
             else:  # If no player won, increment the turn counter and continue the game
                 turns += 1
 
+# TODO add a way for the players to choose what game piece they want
 
 # Begin program
 
-# Print welcome statement
-print('''
-Welcome to Tik Tac Toe!
-Player 1 will be "X"
-Player 2 will be "O"
-Pick a location on the board to place your pieces!
-''')
+while True:
+    # Print welcome statement
+    print('''
+    Welcome to Tik Tac Toe!
+    Player 1 will be "X"
+    Player 2 will be "O"
+    Pick a location on the board to place your pieces!
+    ''')
 
-# Start the game by calling the function
-game_machine()
+    # Start the game by calling the function
+    game_machine()
+
+    # Ask the players if they would like to play again
+    while True:  # This loop is for input validation
+        play_again = input('Would you like to play again? (y/n): ')
+        if play_again == "y" or play_again == "n":
+            break
+        else:
+            print('Please select n or y')
+
+    # This is the same statements as above but is used
+    # to determine whether to replay the game
+    if play_again == "y":
+        continue
+    elif play_again == "n":
+        break
 
 # Print goodbye statement
 print('Thank you for playing!')
