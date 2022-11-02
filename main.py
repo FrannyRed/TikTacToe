92b8077de3cb1464bae3dd073f7c8e923540e20c
Admin <nicholasfran45@gmail.com>
2022-11-01 21:18:58 -0400

    Added and rearranged comments.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Define the game function
 
+# This function accepts the current game array and prints the game board
 def game_board(board_array=[]):
     print('# # # # # # #\n'
           '#', board_array[0], '|', board_array[1], '|', board_array[2], '#\n'
@@ -10,23 +11,30 @@ def game_board(board_array=[]):
           '# # # # # # #\n')
 
 
+# This is the function that runs the processes of the game including
+# player input and calling the game board.
 def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 7, 8, 9]):
     # Print the game board
     game_board(game_array)
 
     # The while statement will keep playing the game until the turns are up
     while turns < 9:
+
         # The for loop alternates between both players
         for i in range(0, 2):
             print('Start turn', turns + 1, ': Player', i + 1, '!')
 
-            # The current player will pick their next move on the board by choosing a number on the board
+            # The current player will pick their next move on the board
+            # with their choice going through the input validation loop.
             while True:
                 selection = input('Select Location : ')
                 if selection.isdigit():
                     if 0 < int(selection) < 10:
-                        game_array[int(selection) - 1] = game_pieces[i]  # The board space is replaced with the players game piece
-                        game_board(game_array)  # Print the changes to game board
+
+                        # The board space is replaced with the players game piece and
+                        # the updated board is printed for the player.
+                        game_array[int(selection) - 1] = game_pieces[i]
+                        game_board(game_array)
                         break
                     else:
                         print('Please select a number on the game board')
@@ -34,7 +42,7 @@ def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 
                     print('Please select a number')
 
             # This next section will check to see if a player won
-
+            # rolling through all the different winning scenarios.
             if game_array[0] == game_array[1] == game_array[2]:  # Check row 1
                 print('Player', game_pieces.index(game_array[0]) + 1, 'wins!!!')
                 return
@@ -59,7 +67,7 @@ def game_machine(turns=0, game_pieces=['X', 'O'], game_array=[1, 2, 3, 4, 5, 6, 
             elif game_array[2] == game_array[4] == game_array[6]:  # Check diagonal top right to bottom left
                 print('Player', game_pieces.index(game_array[2]) + 1, 'wins!!!')
                 return
-            else:  # If no player one, increment the turn counter and continue the game
+            else:  # If no player won, increment the turn counter and continue the game
                 turns += 1
 
 
